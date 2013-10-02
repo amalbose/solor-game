c928078fefe9839b58020372774bc5c8e9f1b3b0
Amal Bose <axatrikx@gmail.com>
2013-10-02 10:34:16 +0530

    Modified Solor class to extend Game. Added AbstractScreen and SplashScreen classes. MenuScreen is dummy

---
+++ solor-game/src/com/axatrikx/solor/screen/SplashScreen.java
@@ -0,0 +1,64 @@
+package com.axatrikx.solor.screen;
+
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.delay;
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.fadeIn;
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.fadeOut;
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.sequence;
+
+import com.axatrikx.solor.Solor;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Action;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Scaling;
+
+public class SplashScreen extends AbstractScreen {
+
+	private Image splashImage;
+
+	public SplashScreen(Solor game) {
+		super(game);
+	}
+
+	@Override
+	public void show() {
+		super.show();
+
+		// start playing the menu music
+		// game.getMusicManager().play(SolorMusic.MENU);
+
+		TextureRegion splashRegion = new TextureRegion(new Texture(
+				"images/splash.png"), 0, 0, 512, 301);
+		Drawable splashDrawable = new TextureRegionDrawable(splashRegion);
+
+		// here we create the splash image actor; its size is set when the
+		// resize() method gets called
+		splashImage = new Image(splashDrawable, Scaling.stretch);
+		splashImage.setFillParent(true);
+
+		// this is needed for the fade-in effect to work correctly; we're just
+		// making the image completely transparent
+		splashImage.getColor().a = 0f;
+
+		// configure the fade-in/out effect on the splash image
+		splashImage.addAction(sequence(fadeIn(0.75f), delay(1.75f),
+				fadeOut(0.75f), new Action() {
+					@Override
+					public boolean act(float delta) {
+						// the last action will move to the next screen
+						game.setScreen(new MenuScreen(game));
+						return true;
+					}
+				}));
+
+		// and finally we add the actor to the stage
+		stage.addActor(splashImage);
+	}
+
+	@Override
+	public void dispose() {
+		super.dispose();
+	}
+}
